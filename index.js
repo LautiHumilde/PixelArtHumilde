4ec6f471402a3a3199ec76647986de44a6d1bd20
LautiHumilde <132624519+LautiHumilde@users.noreply.github.com>
2023-08-01 15:05:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const gris = document.getElementById("gris");
+const rojo = document.getElementById("rojo");
+const marron = document.getElementById("marron");
+const piel = document.getElementById("piel");
+const celeste = document.getElementById("celeste");
+const negro = document.getElementById("negro");
+
+gris.addEventListener("click", function() {
+
+});
+
+rojo.addEventListener("click", function() {
+    
+});
+
+marron.addEventListener("click", function() {
+    
+});
+
+piel.addEventListener("click", function() {
+    
+});
+
+celeste.addEventListener("click", function() {
+    
+});
+
+negro.addEventListener("click", function() {
+    
+});
